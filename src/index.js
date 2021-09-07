43a7f8931782ab98dff5e6cc3c1f431801e8c6a2
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-07 22:22:34 +0200

    Fetch and display drinks from the fetchUrl API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
 import logo from './images/logo.png';
+import heart from './images/heart.svg';
+import drink from './images/drink.PNG';
 
 const logoImg = document.querySelector('.logo');
 logoImg.src = logo;
+const drinkImage = document.querySelectorAll('.card-img-top');
+const drinkTitle = document.querySelectorAll('.card-title');
+const likeButton = document.querySelectorAll('#like');
+
+const fetchUrl = 'https://thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic';
+
+const getDrinks = async () => {
+  const fetchDrinks = await fetch(fetchUrl, {
+    method: 'GET',
+  });
+  const data = await fetchDrinks.json();
+  for (let i = 0; i < drinkImage.length; i += 1) {
+    drinkImage[i].src = data.drinks[i].strDrinkThumb;
+    drinkTitle[i].textContent = data.drinks[i].strDrink;
+  }
+  return data;
+};
+
+getDrinks();
+
+const Items = {
+  drinkImg: drink,
+  drinkTitle: 'Testing',
+  drinkLike: heart,
+  drinklikes: 21,
+  drinkComment: '',
+};
+
+likeButton.forEach((img) => {
+  img.src = heart;
+});
