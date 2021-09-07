28825bae1aa2392943ff312cb748b2c5b5b216bc
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-08 00:17:40 +0200

    Add like feature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+/* eslint-disable no-loop-func */
 import logo from './images/logo.png';
-import heart from './images/heart.svg';
+import heart from './images/unfilled-heart.svg';
+import heartFilled from './images/filled-heart.svg';
 
 const logoImg = document.querySelector('.logo');
 logoImg.src = logo;
 const drinkImage = document.querySelectorAll('.card-img-top');
 const drinkTitle = document.querySelectorAll('.card-title');
+const drinkLikes = document.querySelectorAll('.card-text');
 const likeButton = document.querySelectorAll('#like');
 likeButton.forEach((img) => {
   img.src = heart;
@@ -26,3 +29,18 @@ const getDrinks = async () => {
 };
 
 getDrinks();
+
+let counter = 0;
+
+// drinkLikes.textContent = '$ likes';
+
+drinkLikes.forEach((like) => {
+  like.textContent = `${counter} likes`;
+});
+for (let i = 0; i < likeButton.length; i += 1) {
+  likeButton[i].addEventListener('click', () => {
+    counter += 1;
+    likeButton[i].src = heartFilled;
+    drinkLikes[i].textContent = `${counter} likes`;
+  });
+}
