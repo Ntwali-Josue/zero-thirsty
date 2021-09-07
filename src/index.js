6a53b22871138f9716cf763eb9e34b6ab606e82b
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-07 17:27:50 +0200

    Add footer section

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+const root = document.querySelector('.root');
+const footer = document.createElement('footer');
+
+footer.innerHTML = '<h4>Created by Karangwa ntwali Josue and Ángel Díaz under Microverse</h4>';
+
+root.appendChild(footer);
