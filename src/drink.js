1eedee2b791415e045ada121a56c2ea4a46f0719
Ángel Díaz <diazangel9311@gmail.com>
2021-09-08 10:17:08 -0500

    Remove unnecessary code

--- src/drink.js
+++ src/drink.js
@@ -1,12 +1,4 @@
 class Drink {
-  constructor(category, glass, mainIngridient, iba, drinkID) {
-    this.category = category;
-    this.glass = glass;
-    this.mainIngridient = mainIngridient;
-    this.iba = iba;
-    this.drinkID = drinkID;
-  }
-
   static getDrinkDetails = async (event) => {
     const drinkURL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
     const drinkID = event.target.id.replace('drink', '');
