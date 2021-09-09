f140d73e76bac0010bab47d759914bb1c623111e
Ntwali-Josue <kntwalijos@gmail.com>
2021-09-09 21:21:09 +0200

    Add new test file for likes counter

---
+++ src/test/itemsCounter.test.js
@@ -0,0 +1,16 @@
+// /**
+//  * @jest-environment jsdom
+//  */
+// const main = document.querySelectorAll('.cards');
+// const counter = document.querySelector('.cocktails');
+// console.log(main.length);
+
+// describe('All items counter', () => {
+//   = test('should be defined', () => {
+//     expect(main).not.toBeNull();
+//     expect(counter).not.toBeNull();
+//   });
+//   test('should display total number of items', () => {
+//     expect(counter.innerHTML).toEqual(main.length);
+//   });
+// });
