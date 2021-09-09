a393d2803b11afc9585300b9bf67499443a478e7
Ángel Díaz <diazangel9311@gmail.com>
2021-09-09 13:19:59 -0500

    Add another test for comment counter

--- tests/comment.test.js
+++ tests/comment.test.js
@@ -18,4 +18,12 @@ describe('Test comment counter', () => {
   test('Counts number of comments that were previously created', () => {
     expect(comment.comments.length).toEqual(comment.commentCounter());
   });
+
+  test('Checks that number of comments are never 0', async () => {
+    document.body.innerHTML = commentHTML;
+    comment.getCommenterData();
+    await comment.sendComment('testID2', comment.commenterData);
+    await comment.getComments('testID2');
+    expect(comment.comments.length).toEqual(comment.commentCounter());
+  });
 });
